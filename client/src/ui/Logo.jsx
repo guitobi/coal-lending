8eab500a3dc749200b4637c35e43aec76c998720
guitobi <skullvisit@gmail.com>
2026-03-25 15:57:29 +0200

    feat: optimize font loading and enhance logo component with responsive image support

--- client/src/ui/Logo.jsx
+++ client/src/ui/Logo.jsx
@@ -1,14 +1,17 @@
 function Logo() {
   return (
     <div className="relative px-2 py-2 h-full flex items-center justify-center grow flex-none lg:ml-10 ">
-      <img
-        fetchPriority="high"
-        width={1280}
-        height={1074}
-        src="/Logo-Photoroom.png"
-        alt="Van Share Logo"
-        className="relative z-10 h-14 sm:h-14 md:h-16 lg:h-20 w-auto transition-all duration-300 brightness-0 invert opacity-90 hover:opacity-100"
-      />
+      <picture>
+        <source srcSet="/Logo-Photoroom.webp" type="image/webp" />
+        <img
+          width={1280}
+          height={1074}
+          src="/Logo-Photoroom.png"
+          alt="Van Share Logo"
+          decoding="async"
+          className="relative z-10 h-14 sm:h-14 md:h-16 lg:h-20 w-auto transition-all duration-300 brightness-0 invert opacity-90 hover:opacity-100"
+        />
+      </picture>
     </div>
   );
 }
