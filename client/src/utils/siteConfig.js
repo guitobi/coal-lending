a111a6db14fafa11d790b50693530b1d283cc593
guitobi <skullvisit@gmail.com>
2026-04-02 15:17:02 +0300

    refactor: clean up export statement for SITE_URL in siteConfig.js

--- client/src/utils/siteConfig.js
+++ client/src/utils/siteConfig.js
@@ -2,7 +2,6 @@ const DEFAULT_SITE_URL = "https://vanshare.pl";
 
 const SITE_URL_FROM_ENV = import.meta.env?.VITE_SITE_URL?.replace(/\/+$/, "");
 
-export const SITE_URL =
-  SITE_URL_FROM_ENV || DEFAULT_SITE_URL;
+export const SITE_URL = SITE_URL_FROM_ENV || DEFAULT_SITE_URL;
 
 export const SITE_NAME = "VAN SHARE";
